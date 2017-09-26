0613268226af0525497b1faaa3afd06263fb093f
SmallTest <smalltestagile@gmail.com>
2017-09-26 23:31:20 +0700

    Revert "update build URL"
    This reverts commit cb4e2c1ac9a7bb398f908f53256affce94b697e0.

--- src/main/java/com/smalltest/smalltest/JiraLogService.java
+++ src/main/java/com/smalltest/smalltest/JiraLogService.java
@@ -7,6 +7,7 @@ import hudson.tasks.junit.CaseResult;
 import hudson.tasks.junit.ClassResult;
 import hudson.tasks.junit.SuiteResult;
 import hudson.tasks.junit.TestResult;
+import jenkins.model.Jenkins;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpEntity;
 
@@ -19,7 +20,9 @@ public class JiraLogService {
   public void submitTestLogs(
           String jiraUrl, String username, String password, AbstractBuild build, List<TestResult> testResults)
           throws Exception {
-    String buildUrl = build.getAbsoluteUrl();
+    Jenkins instance = Jenkins.getInstance();
+    String rootUrl = instance == null ? "" : instance.getRootUrl();
+    String buildUrl = rootUrl + build.getUrl();
     Set<String> processedNames = new HashSet<>();
     for (TestResult testResult : testResults) {
       Collection<SuiteResult> suiteResults = testResult.getSuites();
